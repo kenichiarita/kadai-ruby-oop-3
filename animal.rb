06c490013f717174e4d4522e1862ccd6f64ebee9
kenichi.arita <kenichi.arita@kiramex.com>
2021-07-02 09:15:36 +0000

    kadai-ruby-oop-3 review

--- animal.rb
+++ animal.rb
@@ -1,10 +1,9 @@
 class Animal
-  attr_accessor :name, :age, :hobby
+  attr_accessor :name, :age
   
-  def initialize(name, age, hobby)
+  def initialize(name, age)
     self.name   = name
     self.age    = age
-    self.hobby  = hobby
   end
     
   def say
--- human.rb
+++ human.rb
@@ -3,4 +3,12 @@ require './thinkable'
   
 class Human < Animal
   include Thinkable
+
+  attr_accessor :hobby
+
+  def initialize(name, age, hobby)
+    self.name  = name
+    self.age   = age
+    self.hobby = hobby
+  end
 end
